0c419db68c1e75a26e933a3256c68fb4ced8d3b2
Kaktotaks <leonidshevchenko350@gmail.com>
2022-12-07 22:16:26 +0200

    TabBarGroup Controllers setup - done. LoginVC and RegisterVc - done. FireBase account - seted.

---
+++ DnCBasket/Managers/UIManagers/MyAlertManager.swift
@@ -0,0 +1,32 @@
+//
+//  MyAlertManager.swift
+//  DnCBasket
+//
+//  Created by Леонід Шевченко on 06.12.2022.
+//
+
+import UIKit
+
+struct MyAlertManager {
+    static let shared = MyAlertManager()
+
+    private init() { }
+
+    func presentTemporaryInfoAlert(
+        title: String?,
+        message: String?,
+        preferredStyle: UIAlertController.Style,
+        forTime: TimeInterval
+    ) -> UIAlertController {
+        let alertController: UIAlertController = .init(title: title,
+                                                       message: message,
+                                                       preferredStyle: preferredStyle
+        )
+
+        Timer.scheduledTimer(withTimeInterval: forTime,
+                             repeats: false) {_ in
+                alertController.dismiss(animated: true, completion: nil)
+        }
+        return alertController
+    }
+}
